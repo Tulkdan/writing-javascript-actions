e56871884ed0d282bf4bd544e6dc91d80c5e1402
Pedro Correa <pedro8correa@gmail.com>
2020-03-20 23:10:38 -0300

    Fixed misstypo in main.js

--- .github/actions/hello-world/main.js
+++ .github/actions/hello-world/main.js
@@ -1,8 +1,8 @@
 const core = require('@actions/core')
 
-const firstGreeting = core.getInput('first-greetin')
-const secondGreeting = core.getInput('second-greetin')
-const thirdGreeting = core.getInput('third-greetin')
+const firstGreeting = core.getInput('first-greeting')
+const secondGreeting = core.getInput('second-greeting')
+const thirdGreeting = core.getInput('third-greeting')
 
 console.log(`Hello ${firstGreeting}`)
 console.log(`Hello ${secondGreeting}`)
