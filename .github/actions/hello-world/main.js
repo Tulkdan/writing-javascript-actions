677db118f9e208dd645ab371809f6e94360113ef
Pedro Correa <pedro8correa@gmail.com>
2020-03-20 23:05:52 -0300

    allow input in all action files

--- .github/actions/hello-world/main.js
+++ .github/actions/hello-world/main.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+const core = require('@actions/core')
+
+const firstGreeting = core.getInput('first-greetin')
+const secondGreeting = core.getInput('second-greetin')
+const thirdGreeting = core.getInput('third-greetin')
+
+console.log(`Hello ${firstGreeting}`)
+console.log(`Hello ${secondGreeting}`)
+if (thirdGreeting) {
+  console.log(`Hello ${firstGreeting}`)
+}
